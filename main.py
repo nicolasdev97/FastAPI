86b5043f76c1b7cd1f9d0fa713de2974faf62c5d
nicolasdev97 <nicolas.po.im@gmail.com>
2025-02-07 11:36:41 -0500

    Nueva estructura de modelo: Item.

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from models.item_model import Item
 
 #Creacion de una aplicación FastAPI
 app = FastAPI()
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
 
 #Traer datos de algún servidor
 @app.get("/")

1ba74c12202909c527d25d38d8dae9545c52f961
nicolasdev97 <nicolas.po.im@gmail.com>
2025-02-10 10:27:45 -0500

    Ruta para buscar un producto a partir de su id.

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -32,3 +32,13 @@ def crear_producto(producto: Producto):
     producto.id = str(uuid()) # Genera un id unico para el producto
     productos.append(producto)
     return {"mensaje": "Producto creado satisfactoriamente"}
+
+# Buscar productos a partir de un id
+@app.get("/producto/{producto_id}")
+def obtener_producto_por_id(producto_id: str):
+    for producto in productos:
+        if producto.id == producto_id:
+            return producto
+
+    # Si no encuentran un producto con ese id    
+    return {"mensaje": f"El producto con el ID {producto_id} no fue encontrado"}

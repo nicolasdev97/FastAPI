6de1a17cb50fd704141683a7154f966d77719230
nicolasdev97 <nicolas.po.im@gmail.com>
2025-02-10 09:41:06 -0500

    Lectura de todos los productos.

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
 
+# Clase producto
 class Producto(BaseModel):
     id: Optional[str]
     nombre: str
@@ -9,10 +10,17 @@ class Producto(BaseModel):
     precio_venta: float
     proveedor: str
 
+# Se define la API
 app =FastAPI()
 
 productos = []
 
+# Mensaje de bienvenida
 @app.get("/")
 def index():
     return {"mensaje": "Bienvenidos a la API de Productos"}
+
+# Listar productos
+@app.get("/producto")
+def obtener_productos():
+    return productos

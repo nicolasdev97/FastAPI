7d8cab41c0d7f33a4d90c407df68d379b87cd72d
nicolasdev97 <nicolas.po.im@gmail.com>
2025-02-12 15:00:18 -0500

    Validacion valores numericos enteros.

--- s05_tutorial/t07_validations/ex01_path.py
+++ s05_tutorial/t07_validations/ex01_path.py
@@ -4,8 +4,11 @@ app = FastAPI()
 
 # El * al principio de los parametros indica que todos deben ser argumentos llave-valor
 # El ... al principio de Path() indica que es obligatorio
+# El ge=1 indica que debe ser igual o mayor a 1
+# El le=1 indica que debe ser igual o menos a 1
+# El eq=1 indica que debe ser igual a 1
 @app.get("/items/{item_id}")
-async def read_items(*, item_id: int = Path(..., title="Path title example"),
+async def read_items(*, item_id: int = Path(..., title="Path title example", eq=1),
                      q: str | None = Query(default=None, alias="q-alias")):
     results = {"item_id": item_id}
 

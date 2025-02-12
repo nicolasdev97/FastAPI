bef625acbb3e909f007ee1bf48ea7858a8aa83b0
nicolasdev97 <nicolas.po.im@gmail.com>
2025-02-12 14:53:37 -0500

    Argumentos obligatorios y argumentos llave-valor.

--- s05_tutorial/t07_validations/ex01_path.py
+++ s05_tutorial/t07_validations/ex01_path.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI, Query, Path
 
 app = FastAPI()
 
+# El * al principio de los parametros indica que todos deben ser argumentos llave-valor
+# El ... al principio de Path() indica que es obligatorio
 @app.get("/items/{item_id}")
-async def read_items(item_id: int = Path(title="Path title example"),
+async def read_items(*, item_id: int = Path(..., title="Path title example"),
                      q: str | None = Query(default=None, alias="q-alias")):
     results = {"item_id": item_id}
 
